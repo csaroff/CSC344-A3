7526cfd615895ba791b3557d1641d0d1520e611d
csaroff <csaroff@oswego.edu>
2014-10-27 15:13:26 -0400

    The program is complete minus reading in user input.

---
+++ Main.scala
@@ -0,0 +1,106 @@
+sealed abstract class Tree
+case class Const(v: Int) extends Tree {
+  override def toString() = v.toString;
+}
+case class Var(n: String) extends Tree {
+  override def toString() = n;
+}
+case class Sum(l: Tree, r: Tree) extends Tree {
+  override def toString():String =
+  "(" + l.toString() + "+" + r.toString() + ")";
+}
+case class Difference(l:Tree, r:Tree) extends Tree {
+  override def toString():String =
+  "(" + l.toString() + "-" + r.toString() + ")";
+}
+case class Product(l:Tree, r:Tree) extends Tree {
+  override def toString():String =
+  "(" + l.toString() + "*" + r.toString() + ")";
+}
+case class Quotient(l:Tree, r:Tree) extends Tree{
+  override def toString():String =
+  "(" + l.toString() + "/" + r.toString() + ")";
+}
+object Main {
+  type BindingList = String => Int
+  def substituteBindings (bl: BindingList, t: Tree):Tree = t match{
+    case Const(v) => Const(v);
+    case Var(n) => 
+    try{Const(bl(n));}catch{case mE: MatchError => {Var(n);}}
+    //Const(bl(n));
+    case Sum(l, r) => Sum(substituteBindings(bl, l), substituteBindings(bl, r));
+    case Difference(l, r) => Difference(substituteBindings(bl, l), substituteBindings(bl, r));
+    case Product(l, r) => Product(substituteBindings(bl, l), substituteBindings(bl, r));
+    case Quotient(l, r) => Quotient(substituteBindings(bl, l), substituteBindings(bl, r));
+  }
+  def simplify(t: Tree): Tree = t match {
+    case Sum(Const(l), Const(r)) => Const(l + r);
+    case Sum(l, Const(r)) if(r==0) => simplify(l);
+    case Sum(Const(l), r) if(l==0) => simplify(r);
+    case Sum(l, Difference(Const(a), b)) if(a==0 && l==b) => Const(0);
+    case Sum(l, r) 
+      if(simplify(l).isInstanceOf[Const] && simplify(r).isInstanceOf[Const]) 
+        => simplify(Sum(simplify(l), simplify(r)));
+    case Sum(l,r) => Sum(simplify(l), simplify(r));
+  //case Sum(l, r) if (l==r) => Product(Const(2), simplify(l));
+
+    case Difference(Const(l), Const(r)) => Const(l-r);
+    case Difference(l, Const(r)) if(r==0) => simplify(l);
+    case Difference(l, r) if(l==r) => Const(0);
+    case Difference(l, r) 
+      if(simplify(l).isInstanceOf[Const] && simplify(r).isInstanceOf[Const])
+        => simplify(Difference(simplify(l), simplify(r)));
+    case Difference(l, r) => Difference(simplify(l), simplify(r));
+
+    case Product(Const(l), Const(r)) => Const(l*r);
+    case Product(l,Const(r)) if (r==0) => Const(0);
+    case Product(Const(l), r) if (l==0) => Const(0);
+    case Product(l,Const(r)) if (r==1) => simplify(l);
+    case Product(l,r) if (l==Const(1)) => simplify(r);
+    case Product(l,r)
+      if(simplify(l).isInstanceOf[Const]&&simplify(r).isInstanceOf[Const])
+        => simplify(Product(simplify(l), simplify(r)));
+    case Product(l,r) => Product(simplify(l), simplify(r));
+
+    case Quotient(Const(l), Const(r)) => Const(l/r);
+    case Quotient(Const(l), r) if(l==0) => Const(0);
+    case Quotient(l, Const(r)) if(r==1) => simplify(l);
+    case Quotient(l, r) if(l==r) => Const(1);
+    case Quotient(l,r) 
+      if(simplify(l).isInstanceOf[Const]&&simplify(r).isInstanceOf[Const])
+        => simplify(Quotient(simplify(l), simplify(r)));
+    case Quotient(l,r) => Quotient(simplify(l), simplify(r));
+
+    case _ => t;
+  }
+  def main(args: Array[String]):Unit= {
+    var expressions: List[Tree] = List(
+     Sum(Var("x"), Const(0)),
+     Sum(Const(0), Var("x")),
+     Difference(Var("x"), Const(0)),
+     Difference(Var("x"), Var("x")),
+     Sum(Var("x"), Difference(Const(0), Var("x"))),
+     Product(Var("x"), Const(0)),
+     Product(Const(0), Var("x")),
+     Product(Var("x"), Const(1)),
+     Product(Const(1), Var("x")),
+     Quotient(Const(0), Var("x")),
+     Quotient(Var("x"), Const(1)),
+     Quotient(Var("x"), Var("x")),
+     Sum(Var("x"), Product(Var("x"), Difference(Var("y"), Quotient(Var("z"), Const(2))))),
+     Sum(Difference(Var("z"), Const(2)), Product(Var("x"), Const(5))),
+     Sum(Const(1), Var("a")),
+     Product(Sum(Var("a"), Var("b")), Difference(Var("a"), Const(1))),
+     Product(Sum(Var("x"), Var("y")), Quotient(Sum(Const(3), Var("x")), Const(4))),
+     Sum(Difference(Const(1), Var("a")), Const(3))
+     
+    )
+    val bindingList : BindingList = { case "x" => 2 case "z" => 8};
+
+    for(expression <- expressions){
+      val exp = substituteBindings(bindingList, expression);
+      println("Expression: " + expression + "=" + simplify(expression));
+    }
+
+  }
+}
